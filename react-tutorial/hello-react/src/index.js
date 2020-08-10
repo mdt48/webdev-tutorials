d2dbbd4b5f21dd29b30b900411e56cca7987b45f
mdt48 <mdt_@gwu.edu>
2020-08-10 17:24:41 -0400

    render objects from list

--- react-tutorial/hello-react/src/index.js
+++ react-tutorial/hello-react/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 const lakeList = [
-  "Echo Lake",
-  "Maud Lake",
-  "Cascade Lake"
-];
+  {id: "1", name: "Echo", trailhead: "Echo"},
+  {id: "2", name: "Maud", trailhead: "Wrights"},
+  {id: "3", name: "Velma", trailhead: "Bayview"}
+]
 
 function App({lakes}) {
   return (
     <ul>
-      {lakes.map(lake =><li>{lake}</li>)}
+      {lakes.map(lake => (
+        <div>
+          <h2>{lake.name}</h2>
+          <p>Accessed by: {lake.trailhead}</p>
+        </div>
+      ))}
     </ul>
   );
 }
